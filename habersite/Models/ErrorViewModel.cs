3f5f3f3a8aa4ac6e37b04664513e8c63011fae70
AysegulCN <aysegul70coban@gmail.com>
2025-11-10 14:24:51 +0300

    Add project files.

---
+++ habersite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace habersite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
